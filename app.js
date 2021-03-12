cf1248eb308e87945bfdd15ad421fbd10e1beb5a
Brice Schultz <Briceschutz91@gmail.com>
2021-03-11 21:59:13 -0700

    EOD Commit / Created first Model & Schema | TS @ 23:16

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 require("dotenv/config");
+
+//Import Routes
+
+const postsRoute = require("./routes/posts");
+
+app.use("/posts", postsRoute);
+
 //Routes
 
 app.get("/", (req, res) => {
   res.send("We are Home");
-});
-
-app.get("/posts", (req, res) => {
-  res.send("We are on Posts!");
 });
 
 //How we start listening to the Server
--- routes/posts.js
+++ routes/posts.js
@@ -1,3 +1,13 @@
 const express = require("express");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("We are on Posts!");
+});
+
+router.get("/specific", (req, res) => {
+  res.send("We are on a specific post!");
+});
+
+module.exports = router;
